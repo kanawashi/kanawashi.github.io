a25291243c27384c35eaa6ea3c40eb1d25689f1a
kinoa kanawashi <kanawashi@kinoanoMacBook-Air.local>
2024-01-27 14:28:19 +0900

    mavoの動作仕様確認の準備と、画像表示に関するnextJsの設定ファイルの変更

--- app/topicDeck-comp/playmat-parts.tsx
+++ app/topicDeck-comp/playmat-parts.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export interface CardProps {
   children?: React.ReactNode
 }
@@ -21,16 +23,21 @@ export function Card ({title, sentence, empathyDegree, empathizeHandler}: TopicD
   const [empathizedCount, setEmpathizedCount] = empathizeHandler;
 
   return (
-    <div className="h-96 w-full mx-4 p-4 border-gray-800 bg-lime-200 shadow-lg shadow-gray-300 rounded-2xl">
-      <h3 className="text-center text-xl border-b border-black">{title}</h3>
-      <p>{sentence}</p>
-      <button 
-        onClick={() => setEmpathizedCount(empathizedCount + empathyDegree)}
-        className="bg-fuchsia-300 rounded-2xl"
-      >
-        いいねボタン
-      </button>
-    </div>
+    <>
+      <Script src="https://get.mavo.io/mavo.js"/>
+      <div mv-app="topicDeck" mv-storage="https://github.com/kanawashi/kanawashi.github.io/blob/mavo-experiment/database/database.json" mv-autosave="10" className="h-96 w-full mx-4 p-4 border-gray-800 bg-lime-200 shadow-lg shadow-gray-300 rounded-2xl">
+        <h3 className="text-center text-xl border-b border-black">{title}</h3>
+        <p>{sentence}</p>
+        <button 
+          property={title}
+          onClick={() => setEmpathizedCount(empathizedCount + empathyDegree)}
+          className="bg-fuchsia-300 rounded-2xl"
+        >
+          いいねボタン
+        </button>
+      </div>
+    </>
+
   )
 }
 
